962f84f07cd22595e2eebf69d0c5dc088f7f06a4
UP240963 <up240963@alumnos.upa.edu.mx>
2025-06-05 12:20:14 -0600

    Inicio del control de versiones

---
+++ día dos.py
@@ -0,0 +1,7 @@
+Nombres= input( "Escribe tu nombre")
+Apellidos= input ("Escribe tus apellidos")
+Nombre_completo= Nombres+Apellidos
+print(Nombre_completo)
+Pais=input("Escribe tu país de origen")
+Ciudad=input("Escribe una ciudad")
+Edad=input
